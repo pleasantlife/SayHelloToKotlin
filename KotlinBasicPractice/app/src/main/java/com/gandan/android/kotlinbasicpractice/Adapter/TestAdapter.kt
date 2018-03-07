0cc02adf039b1062a1cdcb8b3969f5f7cf2d9635
pleasantlife <pleasantlife@gmail.com>
2018-03-07 23:29:58 +0900

    180307 Kotlin Practice Update.

---
+++ KotlinBasicPractice/app/src/main/java/com/gandan/android/kotlinbasicpractice/Adapter/TestAdapter.kt
@@ -0,0 +1,12 @@
+package com.gandan.android.kotlinbasicpractice.Adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by XPS on 2018-03-07.
+ */
+class TestAdapter {
+}
--- KotlinBasicPractice/app/src/main/java/com/gandan/android/kotlinbasicpractice/MainActivity.kt
+++ KotlinBasicPractice/app/src/main/java/com/gandan/android/kotlinbasicpractice/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.gandan.android.kotlinbasicpractice
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.widget.TextView
 import com.gandan.android.kotlinbasicpractice.Model.Person
 
 
@@ -15,7 +19,15 @@ class MainActivity : AppCompatActivity() // Java : class MainActivity extends Ap
         setContentView(R.layout.activity_main)
         //new 키워드를 사용하지 않고 클래스를 생성한다!
         var person : Person = Person("Peter", 13)
-
+        var txtTest = findViewById<TextView>(R.id.txtTest)
+        txtTest.text = "Hello"
+        txtTest.textSize = 28f
+        txtTest.typeface = Typeface.DEFAULT_BOLD
+        txtTest.setTextColor(Color.parseColor("#000000"))
+        var color = txtTest.textColors
+        Log.e("getText", txtTest.text.toString()+"")
+        Log.e("color", color.toString()+"");
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
     }
 
 }
