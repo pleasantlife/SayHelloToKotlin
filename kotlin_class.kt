1bd3f6b2158fcd6b13cc64d50db429474aea5203
pleasantlife <pleasantlife@gmail.com>
2018-03-12 23:34:09 +0900

    180312 kotlin file update.

---
+++ kotlin_class.kt
@@ -0,0 +1,19 @@
+class Person(name:String, age:Int){
+
+}
+
+data class Family(name:String){
+
+}
+
+class Father() : Person(name, age){
+
+}
+
+class Mother() : Person(name, age){
+
+}
+
+class Child() : Person(name, age){
+    
+}
---
+++ kotlin_variable.kt
@@ -0,0 +1,6 @@
+class Person(name:String, age:Int){
+    var name = "Kotlin Kim"
+    var age = 30
+    var height : Int = 160
+    print(name + age)
+}
